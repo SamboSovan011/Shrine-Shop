5f85b6a919423600b4a7c2e26779082cd7b20d30
SovanSambo <s.ngeth@kokopon.com>
2022-03-25 12:39:35 +0700

    Working on: MDC-103 => on_processing ()

--- lib/app.dart
+++ lib/app.dart
@@ -12,22 +12,53 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'backdrop.dart';
 import 'colors.dart';
+import 'category_menu_page.dart';
 import 'home.dart';
 import 'login.dart';
+
+import 'model/product.dart';
+
 import 'package:flutter/material.dart';
 import 'supplemental/cut_corners_border.dart';
 
 // TODO: Convert ShrineApp to stateful widget (104)
-class ShrineApp extends StatelessWidget {
+class ShrineApp extends StatefulWidget {
   const ShrineApp({Key? key}) : super(key: key);
+
+  @override
+  State<ShrineApp> createState() => _ShrineAppState();
+}
+
+class _ShrineAppState extends State<ShrineApp> {
+
+  Category _currentCategory = Category.all;
+
+  void _onCategoryTap(Category category) {
+    setState(() {
+      _currentCategory = category;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shrine',
       // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
-      home: HomePage(),
+      home: Backdrop(
+        // TODO: Make currentCategory field take _currentCategory (104)
+        currentCategory: Category.all,
+        // TODO: Pass _currentCategory for frontLayer (104)
+        frontLayer: HomePage(),
+        // TODO: Change backLayer field value to CategoryMenuPage (104)
+        backLayer: CategoryMenuPage(
+          currentCategory: _currentCategory,
+          onCategoryTap: _onCategoryTap,
+        ),
+        frontTitle: HomePage(category: _currentCategory),
+        backTitle: Text('MENU'),
+      ),
       // TODO: Make currentCategory field take _currentCategory (104)
       // TODO: Pass _currentCategory for frontLayer (104)
       // TODO: Change backLayer field value to CategoryMenuPage (104)
@@ -54,61 +85,61 @@ class ShrineApp extends StatelessWidget {
 // TODO: Build a Shrine Theme (103)
 final ThemeData _kShrineTheme = _buildShrineTheme();
 
-// ThemeData _buildShrineTheme() {
-//   final ThemeData base = ThemeData.light();
-//   return base.copyWith(
-//     colorScheme: base.colorScheme.copyWith(
-//       primary: kShrinePink100,
-//       onPrimary: kShrineBrown900,
-//       secondary: kShrineBrown900,
-//       error: kShrineErrorRed,
-//     ),
-//     // TODO: Add the text themes (103)
-//     textTheme: _buildShrineTextTheme(base.textTheme),
-//     textSelectionTheme: const TextSelectionThemeData(
-//       selectionColor: kShrinePink100,
-//     ),
-//     // TODO: Add the icon themes (103)
-//     // TODO: Decorate the inputs (103)
-//     inputDecorationTheme: const InputDecorationTheme(
-//       focusedBorder: OutlineInputBorder(
-//         borderSide: BorderSide(
-//           width: 2.0,
-//           color: kShrineBrown900,
-//         ),
-//       ),
-//       border: CutCornersBorder(),
-//     ),
-//     textButtonTheme: const TextButtonThemeData(
-      
-//     )
-//   );
-// }
-
 ThemeData _buildShrineTheme() {
   final ThemeData base = ThemeData.light();
   return base.copyWith(
     colorScheme: base.colorScheme.copyWith(
-      primary: kShrinePurple,
-      secondary: kShrinePurple,
+      primary: kShrinePink100,
+      onPrimary: kShrineBrown900,
+      secondary: kShrineBrown900,
       error: kShrineErrorRed,
     ),
-    scaffoldBackgroundColor: kShrineSurfaceWhite,
+    // TODO: Add the text themes (103)
     textTheme: _buildShrineTextTheme(base.textTheme),
     textSelectionTheme: const TextSelectionThemeData(
-      selectionColor: kShrinePurple,
+      selectionColor: kShrinePink100,
     ),
+    // TODO: Add the icon themes (103)
+    // TODO: Decorate the inputs (103)
     inputDecorationTheme: const InputDecorationTheme(
-      focusedBorder: CutCornersBorder(
+      focusedBorder: OutlineInputBorder(
         borderSide: BorderSide(
           width: 2.0,
-          color: kShrinePurple,
+          color: kShrineBrown900,
         ),
       ),
       border: CutCornersBorder(),
     ),
+    textButtonTheme: const TextButtonThemeData(
+      
+    )
   );
 }
+
+// ThemeData _buildShrineTheme() {
+//   final ThemeData base = ThemeData.light();
+//   return base.copyWith(
+//     colorScheme: base.colorScheme.copyWith(
+//       primary: kShrinePurple,
+//       secondary: kShrinePurple,
+//       error: kShrineErrorRed,
+//     ),
+//     scaffoldBackgroundColor: kShrineSurfaceWhite,
+//     textTheme: _buildShrineTextTheme(base.textTheme),
+//     textSelectionTheme: const TextSelectionThemeData(
+//       selectionColor: kShrinePurple,
+//     ),
+//     inputDecorationTheme: const InputDecorationTheme(
+//       focusedBorder: CutCornersBorder(
+//         borderSide: BorderSide(
+//           width: 2.0,
+//           color: kShrinePurple,
+//         ),
+//       ),
+//       border: CutCornersBorder(),
+//     ),
+//   );
+// }
 
 // TODO: Build a Shrine Text Theme (103)
 TextTheme _buildShrineTextTheme(TextTheme base) {
